cb7cdf889821caea91d0e726f4551eb3ce5a6979
MartinaAG <martygeorgieva7774@gmail.com>
2019-07-02 22:24:12 +0300

    Extending functionality to all layouts in toolbox

--- scripts/toolbox.js
+++ scripts/toolbox.js
@@ -1,22 +1,15 @@
-function addSection() {
-	/*<section>
-		<div class="layout" id="two">
-			<div class="sth">Marti i Bebsi</div>
-			<div class="sth">Bebsi i Martitu</div>
-		</div>
-</section>*/
+function addSection(className, numberOfSth) {
 	var section = document.createElement('section');
 	var layout = document.createElement('div');
 	layout.classList.add('layout');
-	layout.id = "two";
-	var div1 = document.createElement('div');
-	div1.classList.add('sth');
-	div1.innerText = "Obicham te,";
-	var div2 = document.createElement('div');
-	div2.classList.add('sth');
-	div2.innerText = "Martii!!";
-	layout.appendChild(div1, null);
-	layout.appendChild(div2, null);
+	layout.classList.add(className);
+
+	for(var i = 0; i < numberOfSth; i++){
+		var div = document.createElement('div');
+		div.classList.add('sth');
+		layout.appendChild(div, null);
+	}
+	
 	section.appendChild(layout, null);
 	document.body.appendChild(section, null);
 }
