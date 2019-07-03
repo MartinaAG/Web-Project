36b5bd7b52ca367513b54dddf3bd65fd8ce5c8f5
Bonnev <piggov@gmail.com>
2019-07-03 23:35:41 +0300

    Generalized properties logic. Made headers and footers fixed-positioned

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,38 @@
-var headerAndFooter = document.querySelectorAll('header,article,footer');
+var activeProperties = {
+	'docHeader': ['color', 'size'],
+	'docMain': ['color'],
+	'docFooter': ['color', 'size'],
+	isActive: function(property, elementId) {
+		if(Array.isArray(activeProperties[elementId])) {
+			for(var prop in activeProperties[elementId]) {
+				if(activeProperties[elementId][prop] === property) {
+					return true;
+				}
+			}
+		}
+
+		return false;
+	}
+}
+
+var headerAndFooter = document.querySelectorAll('.propertable');
 for(var i = 0; i < headerAndFooter.length; i++) {
 	headerAndFooter[i].addEventListener('mouseenter', (event) => {
 		var target = event.target;
-		var props = document.getElementById('headerFooterProperties');
-		target.appendChild(props);
 
-		props.style.display = "block";
-		var mock = document.getElementById('color-wheel-mock');
+		var properties = document.getElementById('properties');
+		for(var i = 0; i < properties.children.length; i++) {
+			var child = properties.children[i];
+			if(activeProperties.isActive(child.id, target.id)) {
+				child.style.display = "block";
+			} else {
+				child.style.display = "none";
+			}
+		}
+		target.appendChild(properties);
+		properties.style.display = "block";
+
+		var mock = document.getElementById('color');
 		mock.style.backgroundColor = target.style.backgroundColor;
 
 		if(target.style.backgroundColor === 'rgb(0, 0, 0)') {
@@ -14,13 +40,10 @@ for(var i = 0; i < headerAndFooter.length; i++) {
 		} else {
 			mock.style.border = '2px solid black';
 		}
-
-		var size = document.getElementById('size');
-		size.value = 10;
 	});
 
 	headerAndFooter[i].addEventListener('mouseleave', (event) => {
-		document.getElementById('headerFooterProperties').style.display = "none";
+		document.getElementById('properties').style.display = "none";
 	});
 }
 
@@ -29,24 +52,30 @@ document.getElementById('color-wheel').addEventListener('change', (event) => {
 	var target = input.parentElement.parentElement;
 	target.style.backgroundColor = input.value;
 
-	var mock = document.getElementById('color-wheel-mock');
+	var mock = document.getElementById('color');
 	mock.style.backgroundColor = input.value;
 });
 
-document.getElementById('color-wheel-mock').addEventListener('click', (event) => {
+document.getElementById('color').addEventListener('click', (event) => {
 	var input = document.getElementById('color-wheel');
 	input.click();
 });
 
-var size = 10;
 document.getElementById('size').addEventListener('mouseup', (event) => {
 	var size = document.getElementById('size');
 	var target = size.parentElement.parentElement;
 	var height = target.style.height;
+	var height = parseInt(height.substring(0, height.length-1));
+
 	if(size.value > 10) {
-		target.style.height = (parseInt(height.substring(0, height.length-1))+2)+'%';
+		target.style.height = (height + 2)+'%';
 	} else {
-		target.style.height = (parseInt(height.substring(0, height.length-1))-2)+'%';
+		target.style.height = (height - 2)+'%';
+	}
+
+	if(target.tagName === 'HEADER') {
+		var article = document.querySelector('article');
+		article.style.top = target.style.height;
 	}
 
 	size.value = 10;
